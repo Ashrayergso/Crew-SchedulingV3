bb80935df713d42f8751c087e55613b5d0a4aaaf
Ashrayergso <133031666+Ashrayergso@users.noreply.github.com>
2023-08-03 10:18:55 -0400

    Revert "Revert "Build a Django web app for automating crew-to-ship assignments.""

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+"""
+Django app initialization.
+"""
+
+default_app_config = 'app.apps.AppConfig'
---
+++ project/__init__.py
@@ -0,0 +1,5 @@
+"""
+Django project initialization file
+"""
+
+# This file is intentionally left blank and is required for Django to recognize this as a Python package.
